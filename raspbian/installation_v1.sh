cf235cd8bc7e9787c7445e4bdbb4e657df60d845
bhtoscano <benhn.htb@gmail.com>
2020-12-21 20:58:35 -0600

    update and upgrade

--- raspbian/installation_v1.sh
+++ raspbian/installation_v1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
 sudo apt-get install arduino curl nano tree git -y
 sudo apt-get install python python-pip -y
 sudo apt-get install python3-pip -y
