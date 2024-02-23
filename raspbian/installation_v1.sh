9c5c5508c46217001329c0ca9093f33dd092c4e1
Ben <benhn.htb@gmail.com>
2024-02-23 12:35:44 -0600

    Update installation_v1.sh
    nodejs

--- raspbian/installation_v1.sh
+++ raspbian/installation_v1.sh
@@ -4,3 +4,5 @@ sudo apt-get upgrade -y
 sudo apt-get install arduino curl nano tree git -y
 sudo apt-get install python python-pip -y
 sudo apt-get install python3-pip -y
+#### More tools
+sudo apt-get install nodejs -y
